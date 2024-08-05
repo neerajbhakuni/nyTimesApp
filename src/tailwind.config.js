26a5958d7f2d2d08d347dc656c9598a51348ab87
neerajbhakuni <neeraj.bhakuni@nagarro.com>
2024-08-05 10:06:26 +0530

    tailwind config change done

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -13,19 +13,5 @@ module.exports = {
       },
     },
   },
-  variants: {},
   plugins: [],
 };
-
-/*
-      keyframes: {
-        'spin': {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg)' },
-        },
-      },
-      animation: {
-        'spin': 'spin 7s linear infinite', // Adjust the duration as needed
-      },
-
-*/
